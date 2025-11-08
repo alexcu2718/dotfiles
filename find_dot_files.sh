34fe002bedcd2861d206601a4960a507f84d4d1b
Alexander Curtis <alexcu@tutanota.com>
2025-11-08 16:44:13 +0000

    update

--- find_dot_files.sh
+++ find_dot_files.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-# Unwanted exact names or prefixes (no leading dot)
+
 exclude_names="jupyter|yarnrc|polars|tcsh|ipython|ipynb_checkpoints|omnissa|hyper|q3a|industry|godot|quarto|glib|vlc|racket|syncplay|kdedefaults|password-store|aspnet|rnd|virtual_documents|gitconfig|streamlit|valkeycli_history|redhat|z|java"
 
-# Unwanted file extensions (without dot)
+
 exclude_ext="db|js|zwc|save"
 
-# KDE/desktop environment and GUI-related prefixes/names
+
 exclude_desktop="kde|gtk|desktop|plasma|akonadi|xfce4|thunar|deepin|gnome|kai|sway|wayland|xsettingsd|lxqt|lxde|mate|cinnamon|gnome|xfce|lxsession|kwin"
 
 {
-  # From ~/.config (under 1MB)
+
   du -h --max-depth=1 --exclude='.git' ~/.config | awk '$1 ~ /[Kk]/ { $1=""; sub(/^ /, ""); print }'
 
-  # From ~ (exclude .config and .cache)
+
   find ~ -maxdepth 1 -name ".*" ! -name "." ! -name ".." \
     ! -path "$HOME/.config" ! -path "$HOME/.cache" \
     -exec du -h -s {} + | awk '$1 ~ /[Kk]/ { $1=""; sub(/^ /, ""); print }'
@@ -26,15 +26,14 @@ exclude_desktop="kde|gtk|desktop|plasma|akonadi|xfce4|thunar|deepin|gnome|kai|sw
 HOME_PREFIX="$HOME/"
 
 while IFS= read -r item; do
-  # Strip home prefix to get relative path
+
   relpath="${item#$HOME_PREFIX}"
 
   echo "Copying $item to ./$relpath"
 
-  # Make sure target directory exists
+
   mkdir -p "$(dirname "$relpath")"
-  #making sure to quote properly cos of spaces...
-  # Copy recursively (works for both files and directories)
+
   cp -r "$item" "$relpath"
 done < total_dot_files.txt
 
@@ -42,5 +41,5 @@ rm total_dot_files.txt
 
 cp ~/.config/Code/User/settings.json ./vscode-settings.json
 
-# Copy OSX KVM files for macOS emulation
+
 command -v rsync && rsync -av --max-size=2m ~/OSX-KVM/ ./OSX-KVM
