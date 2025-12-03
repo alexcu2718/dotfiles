abd8a4f6afde34c54b1535a126d7e968a1a8e563
Alexander Curtis <alexcu@tutanota.com>
2025-12-03 22:59:58 +0000

    update

--- .config/fish/config.fish
+++
@@ -1,9 +0,0 @@
-if status is-interactive
-   set -Ux CARAPACE_BRIDGES 'zsh,fish,bash,inshellisense' # optional
-    carapace _carapace | source
- # Commands to run in interactive sessions can go here
-	starship init fish | source
-
-       carapace --list | awk '{print $1}' | xargs -I{} touch ~/.config/fish/completions/{}.fish #
-
-end
--- .config/fish/functions/br.fish
+++
@@ -1,1 +0,0 @@
-/home/alexc/.local/share/broot/launcher/fish/br.fish
