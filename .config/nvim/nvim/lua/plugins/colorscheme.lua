b629b84746a236b1ce0df338cb4645c3a521eb47
Alexander Curtis <alexcu@tutanota.com>
2025-12-19 15:58:50 +0000

    update

--- .config/nvim/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,5 @@
 return {
-  -- Original edge colorscheme (commented out)
-  -- {
-  --   'sainnhe/edge',
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     vim.g.edge_enable_italic = true
-  --     vim.cmd.colorscheme('edge')
-  --   end
-  -- },
 
-  -- Monokai Vibrant Rust colorscheme
   {
     name = "monokai-vibrant-rust",
     dir = vim.fn.stdpath("config") .. "/colors",
